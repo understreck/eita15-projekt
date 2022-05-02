af74219aa030755bf6b39cc5e0798cf7266aeabe
underscore <pqfrml@gmail.com>
2022-05-02 13:55:36 +0200

    Extract getting UID from card into function

--- anja-test/anja-test/main.c
+++ anja-test/anja-test/main.c
@@ -6,11 +6,38 @@
  */ 
 
 #define F_CPU 8000000
+#define GREEN 0x04
+#define RED 0x02
+#define BUZZ 0x01;
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include <stdbool.h>
+
 #include "spi.h"
 #include "mfrc522.h"
+
+struct UID {
+	uint8_t data[5];
+};
+
+bool
+get_card(UID* out) {
+	uint8_t response = mfrc522_request(PICC_REQALL, out->data);
+	
+	if(response == CARD_FOUND) {
+		response = mfrc522_get_card_serial(out->data);
+		if(response == CARD_FOUND) {
+			uint8_t checksum = out->data[0] ^ out->data[1] ^ out->data[2] ^ out->data[3];
+					
+			if(checksum == out->data[4]) {
+				return true;
+			}
+		}
+	}
+	
+	return false;
+}
 
 int main(void)
 {
@@ -19,42 +46,10 @@ int main(void)
 		spi_init();
 		mfrc522_init();
 
-		DDRB |= 0x06;
-		uint8_t str[MAX_LEN];
-		uint8_t msg[9];
-		volatile uint16_t crc;
-		uint32_t length;
+		DDRB |= 0x07; //Buzzer and leds
 
-		volatile uint8_t response = mfrc522_read(ComIEnReg);
-		mfrc522_write(ComIEnReg, response|0x20);
-		response = mfrc522_read(DivIEnReg);
-		mfrc522_write(DivIEnReg, response|0x80);
-
-		while(1){
-			response = mfrc522_request(PICC_REQALL, str);
-
-			if(response == CARD_FOUND) {
-
-				response = mfrc522_get_card_serial(str);
-				if(response == CARD_FOUND) {
-					msg[0] = PICC_SElECTTAG;
-					msg[1] = 0x70;
-					for(uint8_t i = 0; i < 5; i++) {
-						msg[2 + i] = str[i];
-					}
-
-					crc = mfrc522_calc_CRC(msg, 8);
-					msg[7] = (uint8_t)crc;
-					msg[8] = (uint8_t)(crc >> 8);
-
-					response = mfrc522_to_card(Transceive_CMD, msg, 9, str, &length);
-				}
-			}
-			else {
-				PORTB &= ~0x06;
-			}
-
-			_delay_ms(500);
+		while(1) {
+			
 		}
     }
 }
