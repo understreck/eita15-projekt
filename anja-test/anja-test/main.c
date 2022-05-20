38ecd412ed9a564cf52850cef56b610348aa2409
Unknown <yomnaalshehabi12@gmail.com>
2022-05-20 16:48:51 +0200

    Add timers

--- anja-test/anja-test/main.c
+++ anja-test/anja-test/main.c
@@ -14,12 +14,50 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
+
 #include <stdbool.h>
 #include <string.h>
 
-#define GREEN (1 << PORTB2)
-#define RED (1 << PORTB1)
-#define BUZZ (1 << PORTB0)
+#define GREEN	_BV(PORTB2)
+#define RED		_BV(PORTB1)
+#define BUZZ	_BV(PORTB0)
+
+#define TIMER_VECTOR TIMER0_COMPA_vect
+
+#define TIMER_INDEX_GREEN	0
+#define TIMER_INDEX_RED		1
+#define TIMER_INDEX_BUZZ	2
+#define TIMER_INDEX_CARD	3
+#define TIMER_INDEX_BUTTON	4
+
+//Values are milliseconds
+#define TIMER_GREEN			50
+#define TIMER_RED			50
+#define TIMER_BUZZ			50
+#define TIMER_CARD			500
+#define TIMER_LAST_INTERACT	4
+
+#define TIMERS_LENGTH 5
+
+uint16_t timers[TIMERS_LENGTH] = {0};
+
+ISR(TIMER_VECTOR) {
+	for(int i = 0; i < TIMERS_LENGTH; i++) {
+		if(timers[i]) timers[i]--;
+	}
+}
+
+void
+timer_init() {
+	TCCR0A	= _BV(WGM01); //CTC waveform generation mode
+	TCCR0B	= _BV(CS02); //Divide clock signal by 256, 31.25kHz
+	
+	OCR0A	= 31; //Gives a clock of ~1kHz
+	
+	TIFR0	^= TIFR0; //Zero TC0 interrupt flag register
+	TIMSK0	= _BV(OCIE0A); //Enable interrupt on OCR0A compare match
+}
 
 void
 init() {
@@ -28,9 +66,19 @@ init() {
 	spi_init();
 	mfrc522_init();
 	kp_init();
+	
+	//Set leds and buzzer pins to outputs and then zero them
+	DDRB |= GREEN | RED | BUZZ;
+	PORTB &= ~(GREEN | RED | BUZZ);
+	
+	timer_init();
+	sei(); //enable interrupts
 }
 
 int main(void)
 {	
 	init();
+	
+	while(1) {}
+	
 }
